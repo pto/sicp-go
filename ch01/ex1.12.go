5b8ff9282128a91bcb4fb8dbb954f853236276c3
Peter Olsen <pto@me.com>
2014-03-30 20:05:56 -0400

    Pascal's triangle

---
+++ ch01/ex1.12.go
@@ -0,0 +1,35 @@
+// Pascal's triangle
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const LINES = 10
+
+func main() {
+	for i := 1; i <= LINES; i++ {
+		fmt.Print(strings.Repeat("  ", LINES-i))
+		for _, v := range triangle(i) {
+			fmt.Printf("%4d", v)
+		}
+		fmt.Println()
+	}
+}
+
+func triangle(n int) []int {
+	if n == 1 {
+		return []int{1}
+	}
+	prev := triangle(n - 1)
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		if i == 0 || i == n-1 {
+			result[i] = 1
+		} else {
+			result[i] = prev[i] + prev[i-1]
+		}
+	}
+	return result
+}
