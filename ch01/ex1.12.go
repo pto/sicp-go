e995ca5292981e28220572dd77652f1af881e9a9
Peter Olsen <pto@me.com>
2014-03-30 20:13:05 -0400

    renaming function

--- ch01/ex1.12.go
+++ ch01/ex1.12.go
@@ -11,18 +11,18 @@ const LINES = 10
 func main() {
 	for i := 1; i <= LINES; i++ {
 		fmt.Print(strings.Repeat("  ", LINES-i))
-		for _, v := range triangle(i) {
+		for _, v := range triangle_line(i) {
 			fmt.Printf("%4d", v)
 		}
 		fmt.Println()
 	}
 }
 
-func triangle(n int) []int {
+func triangle_line(n int) []int {
 	if n == 1 {
 		return []int{1}
 	}
-	prev := triangle(n - 1)
+	prev := triangle_line(n - 1)
 	result := make([]int, n)
 	for i := 0; i < n; i++ {
 		if i == 0 || i == n-1 {
