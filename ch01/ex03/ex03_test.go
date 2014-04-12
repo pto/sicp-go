9fb706b9f6cba1037b6d8679a2bb68e458729d32
Peter Olsen <pto@me.com>
2014-04-12 18:06:35 -0400

    more tests

--- ch01/ex03/ex03_test.go
+++ ch01/ex03/ex03_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test3DistinctValues(t *testing.T) {
+func Test_SumOfSquaresLargestTwo(t *testing.T) {
 	type testDataType struct {
 		a, b, c float64
 		result  float64
@@ -16,7 +16,27 @@ func Test3DistinctValues(t *testing.T) {
 		{2, 1, 3, 13},
 		{2, 3, 1, 13},
 		{3, 1, 2, 13},
-		{3, 2, 1, 13}}
+		{3, 2, 1, 13},
+		{1, 1, 2, 5},
+		{2, 1, 1, 5},
+		{1, 2, 1, 5},
+		{1, 1, 1, 2},
+		{0, 0, 0, 0},
+		{-1, -2, -3, 5},
+		{-1, -3, -2, 5},
+		{-2, -1, -3, 5},
+		{-2, -3, -1, 5},
+		{-3, -1, -2, 5},
+		{-3, -2, -1, 5},
+		{-1, 2, 3, 13},
+		{-1, 0, 1, 1},
+		{-1, 1, 0, 1},
+		{0, 1, -1, 1},
+		{0, -1, 1, 1},
+		{1, 0, -1, 1},
+		{1, -1, 0, 1},
+		{0, 0, 0.5, 0.25},
+		{0, 0.5, 0.5, 0.5}}
 	for _, data := range testData {
 		if ex03.SumOfSquaresLargestTwo(data.a, data.b, data.c) != data.result {
 			t.Errorf("SumOfSquaresLargestTwo(%v, %v, %v) != %v\n",
