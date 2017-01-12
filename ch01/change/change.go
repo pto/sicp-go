4e4aa0eaf6c2a47898520d38aa53a5080ad4defb
Peter Olsen <pto@me.com>
2017-01-12 15:35:21 -0500

    clarifying

--- ch01/change/change.go
+++ ch01/change/change.go
@@ -9,40 +9,40 @@ import (
 	"strings"
 )
 
-const numberOfCoinDenominations = 5
-
-var calls_to_cc int
+var coinValues = []int{1, 5, 10, 25, 50}
+var functionCalls = 0
 
 func main() {
+	programName := filepath.Base(os.Args[0])
 	if len(os.Args) != 2 || strings.HasPrefix(os.Args[1], "-h") {
-		fmt.Printf("usage: %s number-of-cents\n", filepath.Base(os.Args[0]))
+		fmt.Printf("usage: %s number-of-cents\n", programName)
 		os.Exit(1)
 	}
 	amount, err := strconv.Atoi(os.Args[1])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "change:", err)
+		fmt.Fprintln(os.Stderr, "%s:", programName, err)
 		os.Exit(1)
 	}
-	fmt.Printf("%v in %v calls\n", countChange(amount), calls_to_cc)
+	ways := countChange(amount)
+	fmt.Printf("%v ways in %v calls\n", ways, functionCalls)
 }
 
 func countChange(amount int) int {
-	return cc(amount, numberOfCoinDenominations)
+	return cc(amount, len(coinValues))
 }
 
 func cc(amount int, coins int) int {
-	calls_to_cc++
+	functionCalls++
 	switch {
 	case amount == 0:
 		return 1
 	case amount < 0 || coins == 0:
 		return 0
 	default:
-		return cc(amount, coins-1) + cc(amount-firstCoinSize(coins), coins)
+		return cc(amount, coins-1) + cc(amount-firstCoinValue(coins), coins)
 	}
 }
 
-func firstCoinSize(coins int) int {
-	m := map[int]int{1: 1, 2: 5, 3: 10, 4: 25, 5: 50}
-	return m[coins]
+func firstCoinValue(coins int) int {
+	return coinValues[coins-1]
 }
